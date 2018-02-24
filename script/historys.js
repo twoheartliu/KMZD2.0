962f197483bd7755f5cb91293d12e269e93e928e
775659613 <34055175+775659613@users.noreply.github.com>
2018-02-24 09:36:14 +0800

    xgx
    456

--- script/historys.js
+++ script/historys.js
@@ -3,6 +3,7 @@
  */
 apiready = function () {
     var clear = $api.byId('clear');
+    var history = $api.byId('history');
     $api.addEvt(clear, 'click', function () {
         ClearHistory();
     });
@@ -11,48 +12,25 @@ apiready = function () {
 };
 
 function InitHistory() {
+
     var history = $api.byId('history');
     api.getPrefs({
         key: 'historys'
     }, function (ret, err) {
         var historyText = ret.value || '';
         var historyArray = historyText.split(',');
-// <<<<<<< HEAD
-//         var li = '<li class="history" data-index="{index}">{text}<img class="historyImg" src="../image/x.png" alt="" style="float: right;"></li>';
-// =======
         var li = '<li class="history" data-index="{index}">{text}<img class="historyImg" src="../image/x.png" alt="" style="float: right;" onclick="ClearHistory({index})"></li>';
-// >>>>>>> 4d5250a06286e16dec1edeb23e10df7778179d6a
         for (var i = 0; i < historyArray.length; i++) {
             if (historyArray[i].length == 0)
                 continue;
             var tempLi = li.replace(/\{index\}/g, i);
             tempLi = tempLi.replace(/\{text\}/g, decodeURIComponent(historyArray[i]));
             history.appendHTML(tempLi);
-            fnnone();
         }
 
     });
 }
-function fnnone() {
-  var ul = document.getElementById('history');
-  var ul_lis = ul.getElementsByClassName('historyImg');
-  var ul_lisdiv = ul.getElementsByClassName('history');
-  for (var i = 0; i < ul_lis.length; i++) {
-      ul_lis[i].index = i;
-      ul_lis[i].onclick = function() {
-          var j = this.index;
-          // $api.addCls(ul_lisdiv[j], 'active');
-            var text = ul_lisdiv[j].innerHTML;
-            var a = text.substring(-1,10);
-            alert(text);
-            alert(a);
-          // alert(j);
-      }
-  }
-}
-
 function ClearHistory(index) {
-
     var history = $api.byId('history');
     if (!index) {
         history.innerHTML = '';
@@ -73,20 +51,20 @@ function ClearHistory(index) {
                 key: 'historys',
                 value: historyArray.join(',')
             });
+            history.innerHTML = '';
+            InitHistory();
             api.toast({
                 msg: '已清除历史记录'
             });
-            //location.reload();
-            history.innerHTML = '';
-            InitHistory()
         });
     }
 }
 
 function AppendHistory(text) {
     var history = $api.byId('history');
-    var index = history.children.length;
-    var li = '<li class="history" data-index="' + index + '">' + text + '<img src="../image/x.png" alt="" style="float: right;"></li>';
+    var a = history.children.length;
+    var index = a + 1;
+    var li = '<li class="history" data-index="' + index + '">' + text + '<img src="../image/x.png" alt="" style="float: right;" onclick="ClearHistory(' + index + ')"></li>';
     history.appendHTML(li);
 }
 
