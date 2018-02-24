ac14f5f9972120611f65055c5b0be3e724de5f42
zuichu <cuichenguang1314@163.com>
2018-02-24 10:07:43 +0800

    ccg
    12313

--- script/historys.js
+++ script/historys.js
@@ -8,7 +8,7 @@ apiready = function () {
         ClearHistory();
     });
     InitHistory();
-
+    api.parseTapmode();
 };
 
 function InitHistory() {
@@ -19,16 +19,30 @@ function InitHistory() {
     }, function (ret, err) {
         var historyText = ret.value || '';
         var historyArray = historyText.split(',');
-        var li = '<li class="history" data-index="{index}">{text}<img class="historyImg" src="../image/x.png" alt="" style="float: right;" onclick="ClearHistory({index})"></li>';
+        var li = '<li class="history" data-index="{index}">{text}<img class="historyImg" src="../image/x.png" alt="" style="float: right;" tapmode onclick="ClearHistory({index})"></li>';
         for (var i = 0; i < historyArray.length; i++) {
             if (historyArray[i].length == 0)
                 continue;
             var tempLi = li.replace(/\{index\}/g, i);
             tempLi = tempLi.replace(/\{text\}/g, decodeURIComponent(historyArray[i]));
             history.appendHTML(tempLi);
+            fnnone();
+
         }
 
     });
+}
+function fnnone() {
+  var ul = document.getElementById('history');
+  var ul_lis = ul.getElementsByClassName('historyImg');
+  var ul_lisdiv = ul.getElementsByClassName('history')
+  for (var i = 0; i < ul_lis.length; i++) {
+      ul_lis[i].index = i;
+      ul_lis[i].onclick = function() {
+          var j = this.index;
+          $api.addCls(ul_lisdiv[j], 'active');
+      }
+  }
 }
 function ClearHistory(index) {
     var history = $api.byId('history');
@@ -51,8 +65,8 @@ function ClearHistory(index) {
                 key: 'historys',
                 value: historyArray.join(',')
             });
-            history.innerHTML = '';
-            InitHistory();
+            // history.innerHTML = '';
+            // InitHistory();
             api.toast({
                 msg: '已清除历史记录'
             });
@@ -64,8 +78,9 @@ function AppendHistory(text) {
     var history = $api.byId('history');
     var a = history.children.length;
     var index = a + 1;
-    var li = '<li class="history" data-index="' + index + '">' + text + '<img src="../image/x.png" alt="" style="float: right;" onclick="ClearHistory(' + index + ')"></li>';
+    var li = '<li class="history" data-index="' + index + '">' + text + '<img  class="historyImg" src="../image/x.png" alt="" style="float: right;" tapmode onclick="ClearHistory(' + index + ')"></li>';
     history.appendHTML(li);
+    fnnone();
 }
 
 document.addEventListener('click', function (e) {
