89fe93f7517b44bef636846927aa15680c4e49e6
775659613 <34055175+775659613@users.noreply.github.com>
2018-02-26 13:19:33 +0800

    xgx
    1232525

--- script/search1.js
+++ script/search1.js
@@ -129,13 +129,14 @@ function AddHistory(words, fromRoot) {
     api.getPrefs({
         key: 'historys'
     }, function (ret, err) {
+
         var flag = false;//是否增加历史记录
-        var historyText = ret.value || '';
+        var historyText = '' || ret.value  ;
         var historyArray = historyText.split(',');
         for (var i = 0; i < historyArray.length; i++) {
             decodeURIComponent(historyArray[i]) == words && (flag = true);
         }
-        !flag && historyArray.push(encodeURIComponent(words));
+        !flag && historyArray.unshift(encodeURIComponent(words));
         !flag && api.setPrefs({
             key: 'historys',
             value: historyArray.join(',')
