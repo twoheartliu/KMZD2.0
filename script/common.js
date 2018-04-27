71238bec16c28ebc4817514a7c7c6774753f5100
775659613 <34055175+775659613@users.noreply.github.com>
2018-04-27 08:44:06 +0800

    xgx
    123546

--- script/common.js
+++ script/common.js
@@ -2,41 +2,32 @@ var host = 'http://47.100.11.38';
 var version = '2.0';
 var apiUri = '/kmzd/2.0';
 
-apiready = function() {
+// 
+// if(token){
+//   tokens = token;
+// }
 
-  getToken();
-
-}
-// console.log(token);
-function getToken(){
-  // console.log(1111);
-  var fs = api.require('fs');
-  fs.open({
-      path: 'fs://token/token.txt',
-      flags: 'read_write'
-  }, function(ret, err) {
-    // console.log(3333);
-      if (ret.status) {
-          fs.read({
-              fd: ret.fd,
-              offset: 0
-          }, function(ret, err) {
-              if (ret.status) {
-
-
-                // console.log(token);
-                  fnFuZhi(ret.data);
-              } else {
-                  alert(JSON.stringify(err));
-              }
-          });
-      } else {
-          alert(JSON.stringify(err));
-      }
-  });
-
-}
-function fnFuZhi(data_){
-     tokens = data_;
-     return tokens;
-}
+// var fs = api.require('fs');
+// fs.open({
+//     path: 'fs://token/token.txt',
+//     flags: 'read_write'
+// }, function(ret, err) {
+//   // console.log(3333);
+//     if (ret.status) {
+//         fs.read({
+//             fd: ret.fd,
+//             offset: 0
+//         }, function(ret, err) {
+//             if (ret.status) {
+//
+//                  token = ret.data;
+//                 console.log(token);
+//
+//             } else {
+//                 alert(JSON.stringify(err));
+//             }
+//         });
+//     } else {
+//         alert(JSON.stringify(err));
+//     }
+// });
