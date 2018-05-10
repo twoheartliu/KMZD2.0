356549f5ddecfc2b991c4131ec5e973344a59a67
zuichu <cuichenguang1314@163.com>
2018-05-10 18:12:46 +0800

    ccg
    上传

--- script/get_img.js
+++ script/get_img.js
@@ -2,8 +2,11 @@
 
 
 function ccgIfElse(image) {
+  var single = $api.byId('single');
+  var listening_cover = ret.data.linstening_info.listening_cover;
+  var sandBox = listening_cover.substring(0,8);
   if (sandBox == 'sand_box') {
-    var sandBoxs = listening_cover.replace(/sand_box/,'..');
+    var sandBoxs = listening_cover.replace(/sand_box/, '..');
     $api.append(single, '<div class="single_img fl"><img src="'+ sandBoxs +'" alt=""></div><div class="single_l fl"><div class="single_h1" id="tingdanliebiaobiaoti">'+ret.data.linstening_info.listening_name+'</div><div class="single_img_p_span"><div class="single_div_img fl"><img src="'+ret.data.linstening_info.photo+'" alt=""></div><div class="single_img_name fl">'+ret.data.linstening_info.nick_name+'</div></div><div class="single_time"><div class="single_time_img fl"><img src="../image/KM_sc.png" alt=""></div><div class="single_img_num fl">'+ret.data.linstening_info.collection_total+'</div><div class="single_time_img fl"><img src="../image/KM_play.png" alt=""></div><div class="single_img_num fl">'+ret.data.linstening_info.song_total+'</div><div class="single_time_img fl"><img src="../image/read/aloud_py.png" alt=""></div><div class="single_img_num fl">'+ret.data.linstening_info.play_total+'</div></div></div>')
   }else if (listening_cover == '') {
     $api.append(single, '<div class="single_img fl"><img src="../image/my/fnShareImg.jpg" alt=""></div><div class="single_l fl"><div class="single_h1" id="tingdanliebiaobiaoti">'+ret.data.linstening_info.listening_name+'</div><div class="single_img_p_span"><div class="single_div_img fl"><img src="'+ret.data.linstening_info.photo+'" alt=""></div><div class="single_img_name fl">'+ret.data.linstening_info.nick_name+'</div></div><div class="single_time"><div class="single_time_img fl"><img src="../image/KM_sc.png" alt=""></div><div class="single_img_num fl">'+ret.data.linstening_info.collection_total+'</div><div class="single_time_img fl"><img src="../image/KM_play.png" alt=""></div><div class="single_img_num fl">'+ret.data.linstening_info.song_total+'</div><div class="single_time_img fl"><img src="../image/read/aloud_py.png" alt=""></div><div class="single_img_num fl">'+ret.data.linstening_info.play_total+'</div></div></div>')
